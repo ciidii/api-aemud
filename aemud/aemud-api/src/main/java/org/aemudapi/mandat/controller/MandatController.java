07f4af8876aaca52ef4171aea249c4f3a793f520
unknown <booba167967@gmail.com>
2025-10-22 09:33:53 +0000

    feat(mandat): mandat controller router correcte

--- aemud/aemud-api/src/main/java/org/aemudapi/mandat/controller/MandatController.java
+++ aemud/aemud-api/src/main/java/org/aemudapi/mandat/controller/MandatController.java
@@ -14,7 +14,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/mandats")
+@RequestMapping("mandats")
 @RequiredArgsConstructor
 public class MandatController {
 
